a7de05403720c691ba66c31c8b777682aac93e3b
Niklas Buechner <niklas.buechner@viison.com>
2018-09-09 23:02:57 +0200

    Move over gap tests

---
+++ editor-content-management/piece-table/tests/insert.rs
@@ -0,0 +1,133 @@
+extern crate gap;
+
+use std::str::FromStr;
+
+#[test]
+fn insert_at_beginning_small_file() {
+    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+
+    buffer.insert(0,0, &mut "Hello".to_string());
+
+    assert_eq!("Hello<?php
+namespace Hello\\World;
+
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi\";
+    }
+}
+", format!("{}", buffer));
+}
+
+#[test]
+fn insert_at_end_small_file() {
+    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+
+    buffer.insert(18,0, &mut "Hello".to_string());
+
+    assert_eq!("<?php
+namespace Hello\\World;
+
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi\";
+    }
+}
+Hello", format!("{}", buffer));
+}
+
+#[test]
+fn insert_in_the_middle_small_file() {
+    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+
+    buffer.insert(15,18, &mut ", world".to_string());
+
+    assert_eq!("<?php
+namespace Hello\\World;
+
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi, world\";
+    }
+}
+", format!("{}", buffer));
+}
+
+#[test]
+fn insert_with_gap_resize_small_file() {
+    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+
+    buffer.insert(2,0, &mut "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890".to_string());
+
+    assert_eq!("<?php
+namespace Hello\\World;
+123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi\";
+    }
+}
+", format!("{}", buffer));
+}
+
+fn small_file_contents<'a>() -> &'a str {
+    return "<?php
+namespace Hello\\World;
+
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi\";
+    }
+}
+";
+}
