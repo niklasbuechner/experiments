ab8c5eb79c6a82fa317138e85c7d427500469274
Niklas Buechner <niklas.buechner@viison.com>
2018-09-09 14:45:30 +0200

    Add to string test

---
+++ editor-content-management/gap/tests/to_string.rs
@@ -0,0 +1,46 @@
+extern crate gap;
+
+use std::str::FromStr;
+
+#[test]
+fn to_string() {
+    let buffer = gap::GapBuffer::from_str("<?php
+namespace Hello\\World;
+
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi\";
+    }
+}
+").unwrap();
+
+    assert_eq!("<?php
+namespace Hello\\World;
+
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi\";
+    }
+}
+", format!("{}", buffer));
+}
