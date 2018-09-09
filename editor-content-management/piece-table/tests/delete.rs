6436a202b19584a72679963c5a15df8f911034dc
Niklas Buechner <niklas.buechner@viison.com>
2018-09-09 23:03:58 +0200

    Comment out test where build fails

--- editor-content-management/piece-table/tests/delete.rs
+++ editor-content-management/piece-table/tests/delete.rs
@@ -1,116 +1,116 @@
-extern crate gap;
+// extern crate gap;
 
-use std::str::FromStr;
+// use std::str::FromStr;
 
-#[test]
-fn delete_at_beginning_small_file() {
-    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+// #[test]
+// fn delete_at_beginning_small_file() {
+//     let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
 
-    buffer.delete(0,0, 0,1);
+//     buffer.delete(0,0, 0,1);
 
-    assert_eq!("php
-namespace Hello\\World;
+//     assert_eq!("php
+// namespace Hello\\World;
 
-/**
- * Hello world class.
- */
-class HelloWorld
-{
-    /**
-     * Function to say hi.
-     *
-     * @return string
-     */
-    public function hi()
-    {
-        return \"hi\";
-    }
-}
-", format!("{}", buffer));
-}
+// /**
+//  * Hello world class.
+//  */
+// class HelloWorld
+// {
+//     /**
+//      * Function to say hi.
+//      *
+//      * @return string
+//      */
+//     public function hi()
+//     {
+//         return \"hi\";
+//     }
+// }
+// ", format!("{}", buffer));
+// }
 
-#[test]
-fn delete_at_end_small_file() {
-    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+// #[test]
+// fn delete_at_end_small_file() {
+//     let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
 
-    buffer.delete(6,11, 6,15);
+//     buffer.delete(6,11, 6,15);
 
-    assert_eq!("<?php
-namespace Hello\\World;
+//     assert_eq!("<?php
+// namespace Hello\\World;
 
-/**
- * Hello world class.
- */
-class Hello
-{
-    /**
-     * Function to say hi.
-     *
-     * @return string
-     */
-    public function hi()
-    {
-        return \"hi\";
-    }
-}
-", format!("{}", buffer));
-}
+// /**
+//  * Hello world class.
+//  */
+// class Hello
+// {
+//     /**
+//      * Function to say hi.
+//      *
+//      * @return string
+//      */
+//     public function hi()
+//     {
+//         return \"hi\";
+//     }
+// }
+// ", format!("{}", buffer));
+// }
 
-#[test]
-fn delete_in_the_middle_small_file() {
-    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+// #[test]
+// fn delete_in_the_middle_small_file() {
+//     let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
 
-    buffer.delete(17,1, 18,0);
+//     buffer.delete(17,1, 18,0);
 
-    assert_eq!("<?php
-namespace Hello\\World;
+//     assert_eq!("<?php
+// namespace Hello\\World;
 
-/**
- * Hello world class.
- */
-class HelloWorld
-{
-    /**
-     * Function to say hi.
-     *
-     * @return string
-     */
-    public function hi()
-    {
-        return \"hi\";
-    }
-}", format!("{}", buffer));
-}
+// /**
+//  * Hello world class.
+//  */
+// class HelloWorld
+// {
+//     /**
+//      * Function to say hi.
+//      *
+//      * @return string
+//      */
+//     public function hi()
+//     {
+//         return \"hi\";
+//     }
+// }", format!("{}", buffer));
+// }
 
-#[test]
-fn delete_with_gap_resize() {
-    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+// #[test]
+// fn delete_with_gap_resize() {
+//     let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
 
-    buffer.delete(2,0, 18,0);
+//     buffer.delete(2,0, 18,0);
 
-    assert_eq!("<?php
-namespace Hello\\World;
-", format!("{}", buffer));
-}
+//     assert_eq!("<?php
+// namespace Hello\\World;
+// ", format!("{}", buffer));
+// }
 
-fn small_file_contents<'a>() -> &'a str {
-    return "<?php
-namespace Hello\\World;
+// fn small_file_contents<'a>() -> &'a str {
+//     return "<?php
+// namespace Hello\\World;
 
-/**
- * Hello world class.
- */
-class HelloWorld
-{
-    /**
-     * Function to say hi.
-     *
-     * @return string
-     */
-    public function hi()
-    {
-        return \"hi\";
-    }
-}
-";
-}
+// /**
+//  * Hello world class.
+//  */
+// class HelloWorld
+// {
+//     /**
+//      * Function to say hi.
+//      *
+//      * @return string
+//      */
+//     public function hi()
+//     {
+//         return \"hi\";
+//     }
+// }
+// ";
+// }
--- editor-content-management/piece-table/tests/get_at.rs
+++ editor-content-management/piece-table/tests/get_at.rs
@@ -1,17 +1,17 @@
-extern crate gap;
+// extern crate gap;
 
-use std::str::FromStr;
+// use std::str::FromStr;
 
-#[test]
-fn before_gap() {
-    let buffer = gap::GapBuffer::from_str("Before gap -  After gap").unwrap();
+// #[test]
+// fn before_gap() {
+//     let buffer = gap::GapBuffer::from_str("Before gap -  After gap").unwrap();
 
-    assert_eq!('B', buffer.get_at(0, 0));
-}
+//     assert_eq!('B', buffer.get_at(0, 0));
+// }
 
-#[test]
-fn after_gap() {
-    let buffer = gap::GapBuffer::from_str("Before gap -  After gap").unwrap();
+// #[test]
+// fn after_gap() {
+//     let buffer = gap::GapBuffer::from_str("Before gap -  After gap").unwrap();
 
-    assert_eq!('A', buffer.get_at(0, 14));
-}
+//     assert_eq!('A', buffer.get_at(0, 14));
+// }
