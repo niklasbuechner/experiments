0fada8bb1b12d9e7d6c22076cf4b2ab58a0bb073
Niklas Buechner <niklas.buechner@viison.com>
2018-09-09 14:57:06 +0200

    Add insert tests

---
+++ editor-content-management/gap/tests/insert.rs
@@ -0,0 +1,106 @@
+extern crate gap;
+
+use std::str::FromStr;
+
+#[test]
+fn insert_at_beginning_small_file() {
+    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+
+    buffer.insert(0,0, &mut "Hello".to_string());
+
+    assert_eq!("Hello<?php
+namespace Hello\\World;
+
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi\";
+    }
+}
+", format!("{}", buffer));
+}
+
+#[test]
+fn insert_at_end_small_file() {
+    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+
+    buffer.insert(18,0, &mut "Hello".to_string());
+
+    assert_eq!("<?php
+namespace Hello\\World;
+
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi\";
+    }
+}
+Hello", format!("{}", buffer));
+}
+
+#[test]
+fn insert_in_the_middle_small_file() {
+    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+
+    buffer.insert(15,18, &mut ", world".to_string());
+
+    assert_eq!("<?php
+namespace Hello\\World;
+
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi, world\";
+    }
+}
+", format!("{}", buffer));
+}
+
+fn small_file_contents<'a>() -> &'a str {
+    return "<?php
+namespace Hello\\World;
+
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi\";
+    }
+}
+";
+}
