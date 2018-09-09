0362a1d845f92ff3f550b2ddf2e2754e26b2cc36
Niklas Buechner <niklas.buechner@viison.com>
2018-09-09 15:05:20 +0200

    Add gap resize test

--- editor-content-management/gap/tests/insert.rs
+++ editor-content-management/gap/tests/insert.rs
@@ -83,6 +83,33 @@ class HelloWorld
 ", format!("{}", buffer));
 }
 
+#[test]
+fn insert_with_gap_resize_small_file() {
+    let mut buffer = gap::GapBuffer::from_str(small_file_contents()).unwrap();
+
+    buffer.insert(2,0, &mut "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890".to_string());
+
+    assert_eq!("<?php
+namespace Hello\\World;
+123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+/**
+ * Hello world class.
+ */
+class HelloWorld
+{
+    /**
+     * Function to say hi.
+     *
+     * @return string
+     */
+    public function hi()
+    {
+        return \"hi\";
+    }
+}
+", format!("{}", buffer));
+}
+
 fn small_file_contents<'a>() -> &'a str {
     return "<?php
 namespace Hello\\World;
