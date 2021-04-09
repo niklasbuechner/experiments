4d3ad3cefc0241874cd3d9e9d840d8fc3bba99c7
Niklas Büchner <code@niklasbuechner.com>
2021-04-09 10:17:48 +0200

    List all files in zip

--- zip/src/main.rs
+++ zip/src/main.rs
@@ -4,69 +4,71 @@ pub(crate) mod local_file_header;
 
 use central_directory_file_header::CentralDirectoryFileHeader;
 use end_of_central_directory_file_header::EndOfCentralDirectoryRecord;
-use local_file_header::LocalFileHeader;
+// use local_file_header::LocalFileHeader;
 use std::fs::File;
 use std::io::Read;
 use std::io::Result;
+use std::io::Seek;
+use std::io::SeekFrom;
+use std::process::exit;
 
 fn main() -> Result<()> {
     println!("Listing zip contents!");
     let mut file = File::open("./test-data.zip").unwrap();
+    let metadata = file.metadata().unwrap();
 
+    if metadata.is_dir() {
+        println!("The provided file is a directory and therefore can't be opened.");
+        exit(1);
+    }
+
+    let size = metadata.len();
+    file.seek(SeekFrom::Start(size - 22))?;
+
+    let mut signature = [0; 4];
+    file.read(&mut signature)?;
+    let signature_value = u32::from_le_bytes(signature);
+    let end_of_central_directory_file_header;
+    if signature_value == 0x06054b50 {
+        end_of_central_directory_file_header = EndOfCentralDirectoryRecord::new(&mut file)?;
+    } else {
+        println!("Zip file does not end with an end of central directory entry or the entry contains a comment. {:X}", signature_value);
+        exit(1);
+    }
+
+    // Read central directory of files.
+    file.seek(SeekFrom::Start(
+        end_of_central_directory_file_header.offset_start_of_central_directory as u64,
+    ))?;
+    let mut files: Vec<CentralDirectoryFileHeader> = Vec::new();
     loop {
-        let mut signature = [0; 4];
         file.read(&mut signature)?;
-
         let signature_value = u32::from_le_bytes(signature);
-        if signature_value == 0x04034b50 {
-            let file_header = LocalFileHeader::new(&mut file)?;
-            println!("{:#?}", file_header);
 
-            if file_header.uncomporessed_size == 0 {
-                continue;
-            }
-
-            'readContent: loop {
-                let mut file_buffer = vec![0; 1];
-                file.read(&mut file_buffer)?;
-
-                println!("{}", file_buffer[0]);
-
-                if file_buffer[0] == 0 {
-                    break 'readContent;
-                }
-            }
-        // Data descriptor
-        } else if signature_value == 0x08074b50 {
-            let mut data_descriptor_buffer = [0; 12];
-            file.read(&mut data_descriptor_buffer)?;
-            println!(
-                "crc32: {}, compressed size: {}, uncomporessed size: {}",
-                to_u32(&data_descriptor_buffer, 0),
-                to_u32(&data_descriptor_buffer, 4),
-                to_u32(&data_descriptor_buffer, 8)
-            );
-        } else if signature_value == 0x02014b50 {
-            let central_directory_header = CentralDirectoryFileHeader::new(&mut file)?;
-            println!("{:#?}", central_directory_header);
+        if signature_value == 0x02014b50 {
+            let file = CentralDirectoryFileHeader::new(&mut file)?;
+            files.push(file);
         } else if signature_value == 0x06054b50 {
-            let end_of_central_directory_file_header = EndOfCentralDirectoryRecord::new(&mut file)?;
-            println!("{:#?}", end_of_central_directory_file_header);
-        } else {
-            println!("Signature value {:x}", signature_value);
+            // We have read all entry records, break.
             break;
+        } else {
+            println!(
+                "Expected a central directory file header, unexpected signature received: {:X}",
+                signature_value
+            );
+            exit(1);
         }
     }
 
-    Ok(())
-}
+    println!("Files:");
+    files.iter().for_each(|file| {
+        println!(
+            " - {} (Compression mode: {} - saved {} bytes)",
+            file.file_name,
+            file.compression_method,
+            file.uncomporessed_size as i64 - file.compressed_size as i64
+        );
+    });
 
-#[inline(always)]
-fn to_u32(buffer: &[u8; 12], index: usize) -> u32 {
-    return u32::from_le_bytes([
-        buffer[index],
-        buffer[index + 1],
-        buffer[index + 2],
-        buffer[index + 3],
-    ]);
+    Ok(())
 }
