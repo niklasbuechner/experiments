0a67cbcffa795037c96ba321f9e5e07975c55cfd
Niklas Büchner <code@niklasbuechner.com>
2021-04-24 12:57:23 +0200

    Rewrite base64 to rust iterator

--- base64/src/encode.rs
+++ base64/src/encode.rs
@@ -1,65 +1,112 @@
 use super::BASE64_ALPHABET;
+use std::slice::Iter;
+
+struct Base64Encoder<'a, I>
+where
+    I: Iterator<Item = &'a u8>,
+{
+    iterator: I,
+    output_buffer: Vec<char>,
+}
+
+impl<'a, I> Base64Encoder<'a, I>
+where
+    I: Iterator<Item = &'a u8>,
+{
+    pub fn new(byte_iteror: I) -> Self {
+        Base64Encoder {
+            iterator: byte_iteror,
+            output_buffer: Vec::with_capacity(4),
+        }
+    }
+
+    fn fill_output_buffer(&mut self) {
+        let first_byte = self.iterator.next();
+        let second_byte = self.iterator.next();
+        let third_byte = self.iterator.next();
+
+        let mut base64_characters = match (first_byte, second_byte, third_byte) {
+            (Some(first), Some(second), Some(third)) => vec![
+                BASE64_ALPHABET[extract_first_character_bits(*first)],
+                BASE64_ALPHABET[extract_second_character_bits(*first, *second)],
+                BASE64_ALPHABET[extract_third_character_bits(*second, *third)],
+                BASE64_ALPHABET[extract_fourth_character_bits(*third)],
+            ],
+            (Some(first), Some(second), None) => vec![
+                BASE64_ALPHABET[extract_first_character_bits(*first)],
+                BASE64_ALPHABET[extract_second_character_bits(*first, *second)],
+                BASE64_ALPHABET[extract_third_character_bits(*second, 0)],
+                '=',
+            ],
+            (Some(first), None, None) => vec![
+                BASE64_ALPHABET[extract_first_character_bits(*first)],
+                BASE64_ALPHABET[extract_second_character_bits(*first, 0)],
+                '=',
+                '=',
+            ],
+            _ => Vec::new(),
+        };
+
+        self.output_buffer.append(&mut base64_characters);
+    }
+}
+
+impl<'a, I> Iterator for Base64Encoder<'a, I>
+where
+    I: Iterator<Item = &'a u8>,
+{
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.output_buffer.len() == 0 {
+            self.fill_output_buffer();
+        }
+
+        if self.output_buffer.len() == 0 {
+            None
+        } else {
+            Some(self.output_buffer.remove(0))
+        }
+    }
+}
+
+trait Base64Encoding<'a, I>
+where
+    I: Iterator<Item = &'a u8>,
+{
+    fn base64_encode(&'a self) -> Base64Encoder<'a, I>;
+}
+
+impl<'a> Base64Encoding<'a, Iter<'a, u8>> for str {
+    fn base64_encode(&'a self) -> Base64Encoder<'a, Iter<'a, u8>> {
+        Base64Encoder::new(self.as_bytes().iter())
+    }
+}
+
+impl<'a> Base64Encoding<'a, Iter<'a, u8>> for String {
+    fn base64_encode(&'a self) -> Base64Encoder<'a, Iter<'a, u8>> {
+        Base64Encoder::new(self.as_bytes().iter())
+    }
+}
 
 pub fn base64_encode(content: &str) -> String {
-    let characters: &[u8] = content.as_bytes();
-    let mut base64_output = Vec::with_capacity((characters.len() / 3 + 1) * 4);
-
-    let mut counter = 0;
-    while counter + 3 <= characters.len() {
-        let first_base64_character = extract_first_character_bits(characters[counter]);
-        let second_base64_character =
-            extract_second_character_bits(characters[counter], characters[counter + 1]);
-        let third_base64_character =
-            extract_third_character_bits(characters[counter + 1], characters[counter + 2]);
-        let fourth_base64_character = characters[counter + 2] & 0b00111111;
-
-        base64_output.append(&mut vec![
-            BASE64_ALPHABET[first_base64_character as usize],
-            BASE64_ALPHABET[second_base64_character as usize],
-            BASE64_ALPHABET[third_base64_character as usize],
-            BASE64_ALPHABET[fourth_base64_character as usize],
-        ]);
-
-        counter += 3;
-    }
-
-    if counter + 1 == characters.len() {
-        let first_base64_character = extract_first_character_bits(characters[counter]);
-        let second_base64_character = extract_second_character_bits(characters[counter], 0);
-
-        base64_output.append(&mut vec![
-            BASE64_ALPHABET[first_base64_character as usize],
-            BASE64_ALPHABET[second_base64_character as usize],
-            '=',
-            '=',
-        ]);
-    } else if counter + 2 == characters.len() {
-        let first_base64_character = extract_first_character_bits(characters[counter]);
-        let second_base64_character =
-            extract_second_character_bits(characters[counter], characters[counter + 1]);
-        let third_base64_character = extract_third_character_bits(characters[counter + 1], 0);
-
-        base64_output.append(&mut vec![
-            BASE64_ALPHABET[first_base64_character as usize],
-            BASE64_ALPHABET[second_base64_character as usize],
-            BASE64_ALPHABET[third_base64_character as usize],
-            '=',
-        ]);
-    }
-
-    base64_output.into_iter().collect::<String>()
+    content.base64_encode().collect::<String>()
 }
 
-fn extract_first_character_bits(first_byte: u8) -> u8 {
-    (first_byte & 0b1111100) >> 2
+fn extract_first_character_bits(first_byte: u8) -> usize {
+    ((first_byte & 0b1111100) >> 2) as usize
 }
 
-fn extract_second_character_bits(first_byte: u8, second_byte: u8) -> u8 {
-    (first_byte & 0b00000011) << 4 | ((second_byte & 0b11110000) >> 4)
+fn extract_second_character_bits(first_byte: u8, second_byte: u8) -> usize {
+    ((first_byte & 0b00000011) << 4 | ((second_byte & 0b11110000) >> 4)) as usize
 }
 
-fn extract_third_character_bits(second_byte: u8, third_byte: u8) -> u8 {
-    (second_byte & 0b00001111) << 2 | ((third_byte & 0b11000000) >> 6)
+fn extract_third_character_bits(second_byte: u8, third_byte: u8) -> usize {
+    ((second_byte & 0b00001111) << 2 | ((third_byte & 0b11000000) >> 6)) as usize
+}
+
+fn extract_fourth_character_bits(third_byte: u8) -> usize {
+    (third_byte & 0b00111111) as usize
 }
 
 #[test]
